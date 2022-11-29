25700374780c183e3164898d85fa590ac657caf6
khludy <khludy3@gmail.com>
2022-11-29 02:25:01 -0800

    advance+

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+i, j = 122, 26
+while j > 0:
+    if i == 122:
+        print("{:c}".format(i), end="")
+        j, i = j - 1, i - 1
+        continue
+    if i+1 >= 97 and i+1 <= 122:
+        i = i-32
+    elif i+1 >= 65 and i+1 <= 97:
+        i = i+32
+        print("{:c}".format(i), end="")
+        j, i = j-1, i-1
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new_str = ""
+    for idx in range(len(str)):
+        if idx == n:
+            continue
+        new_str += str[idx]
+        return 
